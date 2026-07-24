bab1b870649d668741e60dd90e0d7f9fea5daa02
DishantPrajapat08 <dishantprajapat08>
2026-07-24 21:55:41 +0530

    sum of all no  from 1 to n which are divisible by 3

--- dsa1.cpp
+++ dsa1.cpp
@@ -1,1 +1,22 @@
 //sum of all no  from 1 to n which are divisible by 3
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int sum=0;
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+
+    for(int i=1;i<=a;i++)
+    {
+        if(i%3==0)
+        {
+            sum+=i;
+        }
+    }
+
+     cout<<sum;
+
+    return 0;
+}
