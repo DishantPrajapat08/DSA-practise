bc18bfc4d794224109473aab07cff8517deb3af0
DishantPrajapat08 <dishantprajapat08>
2026-07-24 22:01:05 +0530

    print factorial of a no n

--- dsa2.cpp
+++ dsa2.cpp
@@ -1,1 +1,18 @@
 //print factorial of a no n
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+
+    for(int i=n;i>=1;i--)
+    {
+     cout<<i<<" ";
+
+    }
+
+    return 0;
+}
