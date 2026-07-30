26c9a5dca58b98a9c9e7e89393fca3d55436d1e7
DishantPrajapat08 <dishantprajapat08>
2026-07-30 22:25:33 +0530

    h2

---
+++ dsa4.cpp
@@ -0,0 +1,19 @@
+// Homework
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    char m = 'A';
+    for (int l = 0; l < n; l++)
+    {
+        for (int i=0;i<=l;i++)
+        {
+            cout<<m;
+        }
+        m+=1;
+        cout<<endl;
+    }
+    return 0;
+}
