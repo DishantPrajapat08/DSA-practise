2a80ab5d3459dad3de2b5456edf62faa3df658a4
DishantPrajapat08 <dishantprajapat08>
2026-08-02 13:13:49 +0530

    upper layer butterfy

--- dsa8.cpp
+++ dsa8.cpp
@@ -11,6 +11,14 @@ int main()
         {
             cout << "*";
         }
+        for (int j = 0; j < 2*(n-1)-2*i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
         cout << endl;
     }
 
